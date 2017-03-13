347b04455379f6bb8af5a1bc97ed6e2c27e84abc
Erkur <eiki@live.com>
2017-03-13 18:55:38 +0000

    Add files via upload

---
+++ capitalize.py
@@ -0,0 +1,3 @@
+#Breytir fyrsta stafnum í hástaf
+x = "foobar"
+print (x.capitalize()) #skilar "Foobar"
---
+++ casefold.py
@@ -0,0 +1,4 @@
+#Breytir hástafi í lágstafi og breytir sérstökum stöfum.
+#Sem dæmi myndi þetta breyta þýska stafinum "ß" sem jafngildir "ss" í "ss"
+x = "FOOßar"
+print (x.casefold()) #skilar "foossar"
