10b3c0277bd779a6c06c6838fc5a3ff54e0f4192
erkur <eiki@live.com>
2017-03-14 14:22:36 +0000

    more

---
+++ strengjavinnsla/istitle.py
@@ -0,0 +1,8 @@
+#skilar True ef fyrsti stafurinn í strengnum er hástafur
+x = "Foobar"
+y = "foobar"
+z = "fooBar"
+
+print (x.istitle()) #skilar True
+print (y.istitle()) #skilar False
+print (z.istitle()) #skilar False
